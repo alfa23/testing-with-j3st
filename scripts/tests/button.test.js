2706b99a6d1842527cffce3ea034ba3d83fa9138
alfa23 <paw235@gmail.com>
2022-04-19 20:26:59 +0000

    DOM testing

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -1,17 +1,23 @@
 /**
- * @jest-environment jsdom
- */
+* @jest-environment jsdom
+*/
 
- const buttonClick = require("../button");
+const buttonClick = require("../button");
 
- beforeAll(() => {
-     document.body.innerHTML = "<p id='par'></p>";
- });
+beforeEach(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
  
- describe("DOM tests", () => {
-     test("Expects content to change", () => {
-         buttonClick();
-         expect(document.getElementById("par")
-             .innerHTML).toEqual("You Clicked");
-     });
- });
+describe("DOM tests", () => {
+    test("Expects content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
+});
